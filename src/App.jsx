c4b6d62a7e62c5c9980274ebc3c5de9f7d16eae2
avidrucker <avidrucker@gmail.com>
2023-07-30 13:13:57 -0400

    fix tidy headings, + whitespace for readability

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,12 @@ function App() {
 
     return (
 	<>
+	    
 	    <h1>React Katas</h1>
+
 	    
 	    <section>
+
 		<h2>1.1. Button & Counter w/ useState</h2>
 		<p>Declare a state variable named count in a functional component and set its initial value to 0.</p>
 		<p>State Update & Event Handling: Add an onClick event to a button that updates the count state variable by incrementing it by 1.</p>
@@ -28,20 +31,28 @@ function App() {
 
 		<button onClick={() => setCount((count) => 0)}>Reset count to zero</button>
 		<p>count is {count}</p>
+
 	    </section>
 
+	    
 	    <section>
+
 		<h2>1.2. Conditional Rendering</h2>
 		<p>Conditional Rendering: Conditionally render a &lt;p&gt; element that says "Counter is even" if counter is an even number.</p>
 		{isEven(count) && <p>count is even</p>}
+
 	    </section>
+
 	    
 	    <section>
-		<h2>1.3. Props Passing</h2>
+
+		<h2>1.3. Props Passing & Props Destructuring</h2>
 		<p>Passing Props: Pass a prop named message to a child component and render it inside a &lt;p&gt; tag.</p>
 		<MessageComponent message="Hi, I am a message." />
 		<MessageComponent2 message="I'm also a message, passed via destructured props." />
+		
 	    </section>
+	    
 	</>
     )
 }
