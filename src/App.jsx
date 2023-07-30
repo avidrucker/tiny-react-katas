fbe12066f64b239a7a11105c5dbd9d29134ba852
avidrucker <avidrucker@gmail.com>
2023-07-30 12:50:44 -0400

    add reset button for counter

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,11 @@ function App() {
 		<h2>1.1. button & counter w/ useState</h2>
 		<p>Declare a state variable named count in a functional component and set its initial value to 0.</p>
 		<button onClick={() => setCount((count) => count + 1)}>
-		    count is {count}
+		    Increment count by one
 		</button>
+
+		<button onClick={() => setCount((count) => 0)}>Reset count to zero</button>
+		<p>count is {count}</p>
 	    </section>
 	</>
     )
