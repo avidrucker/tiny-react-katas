e2eb0b0bd452f0ab6233c0b3818a6d84c1096227
avidrucker <avidrucker@gmail.com>
2023-07-30 16:32:40 -0400

    add border wrapper kata, borders to all katas

--- src/App.jsx
+++ src/App.jsx
@@ -96,6 +96,16 @@ const EffectComponent = () => {
 };
 
 
+// 1.9
+const BorderWrapper = (props) => {
+  return (
+    <div style={{border: "1px solid black"}}>
+      {props.children}
+    </div>
+  );
+};
+
+
 function App() {
 
     const [count, setCount] = useState(0)
@@ -116,12 +126,15 @@ function App() {
 		<h2>1.1. Button & Counter w/ useState</h2>
 		<p>Declare a state variable named count in a functional component and set its initial value to 0.</p>
 		<p>State Update & Event Handling: Add an onClick event to a button that updates the count state variable by incrementing it by 1.</p>
+
+		<BorderWrapper>
 		<button onClick={() => setCount((count) => count + 1)}>
 		    Increment count by one
 		</button>
 
 		<button onClick={() => setCount((count) => 0)}>Reset count to zero</button>
 		<p>count is {count}</p>
+		</BorderWrapper>
 	    </section>
 	    
 	    <hr />
@@ -129,7 +142,12 @@ function App() {
 	    <section>
 		<h2>1.2. Conditional Rendering</h2>
 		<p>Conditional Rendering: Conditionally render a &lt;p&gt; element that says "Counter is even" if counter is an even number.</p>
+		<BorderWrapper>
+		    <div>
+			<p>Conditional text will render below:</p>
 		{isEven(count) && <p>count is even</p>}
+		    </div>
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -137,9 +155,9 @@ function App() {
 	    <section>
 		<h2>1.3. Props Passing & Props Destructuring</h2>
 		<p>Passing Props: Pass a prop named message to a child component and render it inside a &lt;p&gt; tag.</p>
-		<MessageComponent message="Hi, I am a message." />
+		<BorderWrapper><MessageComponent message="Hi, I am a message." /></BorderWrapper>
 		<p>Destructuring Props: Destructure the message prop in the child component and render it.</p>
-		<MessageComponent2 message="I'm also a message, passed via destructured props." />
+		<BorderWrapper><MessageComponent2 message="I'm also a message, passed via destructured props." /></BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -147,10 +165,12 @@ function App() {
 	    <section>
 		<h2>1.4. Mapping Arrays & Lists</h2>
 		<p>Mapping Arrays: Given an array of integers, render each integer inside a &lt;li&gt; element using the map function.</p>
+		<BorderWrapper>
 		<ul style={{"textAlign": "left"}}>
 		    {["a", "b", "c", "d"].map((x, i) =>
 			<li key={i}>{x}</li>)}
 		</ul>
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -158,7 +178,9 @@ function App() {
 	    <section>
 		<h2>1.5. Fragments</h2>
 		<p>React Fragments: Wrap two &lt;p&gt; elements with a React Fragment.</p>
+		<BorderWrapper>
 		<FragmentWrappedComp />
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -166,7 +188,9 @@ function App() {
 	    <section>
 		<h2>1.5. Inline Styling</h2>
 		<p>Inline Styling: Add inline CSS to a &lt;p&gt; to set its color to blue.</p>
+		<BorderWrapper>
 		<p style={{"color": "blue"}}>I am styled to be blue.</p>
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -174,8 +198,10 @@ function App() {
 	    <section>
 		<h2>1.6. Passing Functions as Props</h2>
 		<p>Function as Props: Pass a function as a prop to a child component that updates the counter state in the parent component.</p>
+		<BorderWrapper>
 		<ChildComponent incFunc={incrementFunction} />
 		count is {count}
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
@@ -184,23 +210,34 @@ function App() {
 		<h2>1.7. Working With Arrays in State</h2>
 		<p>Array State: Create an array as a state in a functional component. </p>
 		<p>Array State Update: Add a new item to the array state using the spread operator.</p>
+		<BorderWrapper>
 		<ArrayStateComp />
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
 	    
 	    <section>
-		<h2>useEffect Hook</h2>
+		<h2>1.8. useEffect Hook</h2>
 		<p>UseEffect Hook: Use the useEffect hook to log a message to the console when the component mounts and unmounts.</p>
+		<BorderWrapper>
+		    <p>See console log for print-outs</p>
 		<button onClick={() => setToggle((toggle) => !toggle)}>{toggle ? "Hide EffectComponent" : "Show EffectComponent"}</button>
 				 {toggle && <EffectComponent />}
+		</BorderWrapper>
 	    </section>
 
 	    <hr />
 	    
 	    <section>
-		<h2></h2>
-		<p></p>
+		<h2>1.9. Wrapper Component</h2>
+		<p>Wrapper Component: Make a component that takes in other components as children, and simply renders a black 1px solid border around them.</p>
+		<BorderWrapper>
+		    <p>I am text wrapped by a BorderWrapper component.</p>
+		</BorderWrapper>
+	    </section>
+
+	    <hr />
 	    </section>
 
 	</>
